be0ade755e989bb94b338f8e63af63c188f3b6e2
Carlos Daniel <carlos.daniel@devsquad.email>
2021-08-07 15:17:42 -0300

    feat: storing builds and pipelines

--- src/handlers/gitlab.js
+++ src/handlers/gitlab.js
@@ -1,11 +1,17 @@
+import { GitlabBuild } from '../models/gitlab/build';
 import { GitlabCommit } from '../models/gitlab/commit';
+import { GitlabPipeline } from '../models/gitlab/pipeline';
+import { buildTransformer, pipelineTransformer } from '../utils/transformer';
 
 export const gitlab = async event => {
-  const { event_name, commits } = JSON.parse(event.body);
+  const payload = JSON.parse(event.body);
+  const { object_kind } = payload;
 
-  if (event_name === 'push') {
-    for (const commit of commits) await GitlabCommit.create(commit);
-  }
+  if (object_kind === 'push') for (const commit of payload.commits) await GitlabCommit.create(commit);
+
+  else if (object_kind === 'build') await GitlabBuild.create(buildTransformer(payload));
+
+  else if (object_kind === 'pipeline') await GitlabPipeline.create(pipelineTransformer(payload));
 
   return {
     statusCode: 200,
