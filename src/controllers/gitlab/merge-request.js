057bb0533e9c2d36296bf1651e3266bc074d676b
Carlos Daniel <carlos.daniel@devsquad.email>
2021-08-17 21:56:25 -0300

    feat: store MR in ddb schema and send message in discord

--- src/controllers/gitlab/merge-request.js
+++ src/controllers/gitlab/merge-request.js
@@ -1,5 +1,7 @@
 import debug from 'debug';
 import { GitlabMergeRequest } from '../../models/gitlab/merge-request';
+import { gitlabEmbed } from '../../views/discord-embed';
+import { sendDiscordEmbedMessage } from '../../services/discord';
 
 const logger = debug(`${process.env.PROJECT_NAME}:controllers:gitlab:merge-request`);
 
@@ -12,11 +14,29 @@ export const handleGitlabMergeRequest = async payload => {
     changes,
   } = payload;
 
-  const response = await GitlabMergeRequest.create({
-    ...mergeRequest,
-    user,
-    project,
-    labels,
-    changes,
+  const mergeRequestExists = GitlabMergeRequest.get(mergeRequest.id);
+
+  if (!mergeRequestExists) {
+    const createMergeRequestResponse = await GitlabMergeRequest.create({
+      ...mergeRequest,
+      user,
+      project,
+      labels,
+      changes,
+    });
+    logger('createMergeRequestResponse', createMergeRequestResponse);
+  } else {
+    await GitlabMergeRequest.update({
+      ...mergeRequest,
+    });
+  }
+
+  const content = `${user.name} ${mergeRequest.action} merge request [!${mergeRequest.iid} ${mergeRequest.title}](${mergeRequest.url}) in [${project.namespace}/${project.name}](${project.web_url})`;
+  const message = gitlabEmbed({ user, color: 0xE67E22, content });
+  logger('message', message);
+
+  const { data } = await sendDiscordEmbedMessage({
+    embeds: [message],
   });
+  logger('data %O', data);
 };
