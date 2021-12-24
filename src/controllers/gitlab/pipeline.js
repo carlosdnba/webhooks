65de2c57a3f485b2c86445ba155c360599bd5a74
Carlos Daniel <carlos.daniel@devsquad.email>
2021-12-24 18:45:15 -0300

    feat: start storing data using single table model
    since i changed the linter to standard, vscode also changed a lot of lines with the previous style

--- src/controllers/gitlab/pipeline.js
+++ src/controllers/gitlab/pipeline.js
@@ -1,9 +1,9 @@
-import debug from 'debug';
-import { GitlabPipeline } from '../../models/gitlab/pipeline';
-import { sendDiscordEmbedMessage } from '../../services/discord';
-import { gitlabEmbed } from '../../views/discord-embed';
+import debug from 'debug'
+import { Model, keys } from '../../models/git'
+import { sendDiscordEmbedMessage } from '../../services/discord'
+import { gitlabEmbed } from '../../views/discord-embed'
 
-const logger = debug(`${process.env.PROJECT_NAME}:controllers:gitlab:pipeline`);
+const logger = debug(`${process.env.PROJECT_NAME}:controllers:gitlab:pipeline`)
 
 export const handleGitlabPipeline = async payload => {
   const {
@@ -11,53 +11,59 @@ export const handleGitlabPipeline = async payload => {
     user,
     project,
     commit,
-    merge_request,
-  } = payload;
+    merge_request
+  } = payload
 
   // building and sending message in discord channel
-  let color = '';
-  let content = '';
+  let color = ''
+  let content = ''
 
   if (pipeline.status === 'running') {
-    content = `[${project.name}](${project.web_url}): Pipeline [#${pipeline.id}](${project.web_url}/-/pipelines/${pipeline.id}) of branch [${pipeline.ref}](${project.web_url}/commits/${pipeline.ref}) by [${user.name}](https://gitlab.com/${user.username}) has started running`;
-    color = 0xE67E22;
+    content = `[${project.name}](${project.web_url}): Pipeline [#${pipeline.id}](${project.web_url}/-/pipelines/${pipeline.id}) of branch [${pipeline.ref}](${project.web_url}/commits/${pipeline.ref}) by [${user.name}](https://gitlab.com/${user.username}) has started running`
+    color = 0xE67E22
   } else if (pipeline.status === 'success') {
-    content = `[${project.name}](${project.web_url}): Pipeline [#${pipeline.id}](${project.web_url}/-/pipelines/${pipeline.id}) of branch [${pipeline.ref}](${project.web_url}/commits/${pipeline.ref}) by [${user.name}](https://gitlab.com/${user.username}) has passed in ${new Date(pipeline.duration * 1000).toISOString().substr(14, 5)}`;
-    color = 0x1F8B4C;
+    content = `[${project.name}](${project.web_url}): Pipeline [#${pipeline.id}](${project.web_url}/-/pipelines/${pipeline.id}) of branch [${pipeline.ref}](${project.web_url}/commits/${pipeline.ref}) by [${user.name}](https://gitlab.com/${user.username}) has passed in ${new Date(pipeline.duration * 1000).toISOString().substr(14, 5)}`
+    color = 0x1F8B4C
   } else if (pipeline.status === 'failed') {
-    content = `[${project.name}](${project.web_url}): Pipeline [#${pipeline.id}](${project.web_url}/-/pipelines/${pipeline.id}) of branch [${pipeline.ref}](${project.web_url}/commits/${pipeline.ref}) by [${user.name}](https://gitlab.com/${user.username}) has failed in ${new Date(pipeline.duration * 1000).toISOString().substr(14, 5)}`;
-    color = 0xE74C3C;
+    content = `[${project.name}](${project.web_url}): Pipeline [#${pipeline.id}](${project.web_url}/-/pipelines/${pipeline.id}) of branch [${pipeline.ref}](${project.web_url}/commits/${pipeline.ref}) by [${user.name}](https://gitlab.com/${user.username}) has failed in ${new Date(pipeline.duration * 1000).toISOString().substr(14, 5)}`
+    color = 0xE74C3C
   }
 
-  const message = gitlabEmbed({ user, color, content });
-  logger('message', message);
+  const message = gitlabEmbed({ user, color, content })
+  logger('message %o', message)
 
-  const { data } = await sendDiscordEmbedMessage({
-    embeds: [message],
-  });
-  logger('data %O', data);
+  const response = await sendDiscordEmbedMessage({
+    embeds: [message]
+  })
+  logger('response %j', response)
 
   // storing pipeline info in ddb schema
-  const pipelineExists = await GitlabPipeline.get(pipeline.id);
+  const [pipelineExists] = await Model.query({
+    pk: keys.pipeline,
+    sk: pipeline.id.toString()
+  }).exec()
+  logger('pipelineExists %j', pipelineExists)
 
   if (pipelineExists) {
-    const pipelineUpdateResponse = await GitlabPipeline.update({
-      id: pipeline.id,
+    pipelineExists.pipeline = {
+      ...pipelineExists.pipeline,
       status: pipeline.status,
       detailed_status: pipeline.detailed_status,
       created_at: pipeline.created_at,
       finished_at: pipeline.finished_at,
-      duration: pipeline.duration,
-    });
-    logger('pipelineUpdateResponse %O', pipelineUpdateResponse);
+      duration: pipeline.duration
+    }
+    await pipelineExists.save()
   } else {
-    const pipelineCreateResponse = await GitlabPipeline.create({
-      ...pipeline,
+    const pipelineCreateResponse = await Model.create({
+      pk: keys.pipeline,
+      sk: pipeline.id.toString(),
+      pipeline,
       user,
       project,
       commit,
-      merge_request,
-    });
-    logger('pipelineCreateResponse %O', pipelineCreateResponse);
+      merge_request
+    })
+    logger('pipelineCreateResponse %j', pipelineCreateResponse)
   }
-};
+}
