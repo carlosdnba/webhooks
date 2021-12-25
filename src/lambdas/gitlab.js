ecd7d49bf4a4e5e95f492b03690b4578178c8eed
Carlos Daniel <carlos.daniel@devsquad.email>
2021-12-25 00:40:29 -0300

    feat: throw event for each commit and use another lambda to store commits on database

--- src/lambdas/gitlab.js
+++ src/lambdas/gitlab.js
@@ -1,20 +1,35 @@
-import debug from 'debug'
-import {
-  handleGitlabPush,
-  handleGitlabPipeline
-} from '../controllers/gitlab'
-
-const logger = debug(`${process.env.PROJECT_NAME}:handlers:gitlab`)
+import { handleGitlabPipeline } from '../controllers/gitlab'
+import { putEvent } from '../services/event-bridge'
+import { debug } from '../core/debug'
+import { Model, keys } from '../models/git'
 
 export const gitlab = async event => {
   const payload = JSON.parse(event.body)
-  const { object_kind } = payload
-  logger('payload %j kind %o', payload, object_kind)
+  const { object_kind: kind } = payload
+  debug('handlers:gitlab')('payload %j kind %o', payload, kind)
 
-  if (object_kind === 'push') await handleGitlabPush(payload)
-  else if (object_kind === 'pipeline') await handleGitlabPipeline(payload)
+  if (kind === 'push') {
+    // sending each commit to event bridge so a separate lambda can store it to ddb
+    const promises = payload.commits.map(commit => putEvent('GitLab code was committed', {
+      commit,
+      project: payload.project
+    }))
+    debug('handlers:gitlab')('promises.length %o', promises.length)
+    await Promise.all(promises)
+  } else if (kind === 'pipeline') await handleGitlabPipeline(payload)
 
   return {
     statusCode: 200
   }
+}
+
+export const commit = async event => {
+  const payload = event.detail
+  debug('handlers:commit')('payload %o', payload)
+  await Model.create({
+    pk: keys.commit,
+    sk: payload.commit.id,
+    commit: payload.commit,
+    project: payload.project
+  })
 }
